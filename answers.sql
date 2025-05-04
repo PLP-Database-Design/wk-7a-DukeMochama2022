159744f8dcf1110893427f5f643809e1b7851607
DukeMochama2022 <dukemochama21@gmail.com>
2025-05-04 09:16:33 +0300

    Rectified the code

--- answers.sql
+++ answers.sql
@@ -1,70 +1,47 @@
+Week 7 Assignment Guide
 
--- Question One 1NF
-
--- Problem: The Products column contains Multiple values separated by comma which violates the INF
--- We should ensure that each value is atomic in a column.
-
-CREATE TABLE ProductDetail(
-orderID INT,
-CustomerName VARCHAR(100),
-ProductName VARCHAR(100)
+ --  Question 1
+CREATE TABLE ProductDetail (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Products VARCHAR(100)
 );
 
-INSERT INTO ProductDetail(orderID,CustomerName,ProductName)
-VALUES (101, 'John Doe', 'Laptop'),
+INSERT INTO ProductDetail(OrderID, CustomerName, Products)
+VALUES
+(101, 'John Doe', 'Laptop'),
 (101, 'John Doe', 'Mouse'),
 (102, 'Jane Smith', 'Tablet'),
 (102, 'Jane Smith', 'Keyboard'),
 (102, 'Jane Smith', 'Mouse'),
 (103, 'Emily Clark', 'Phone');
 
--- Conclusion: Each row represents a single column now 
-
--- Question Two  2NF
--- Composite Key----> (OrderID,Product)
--- Partial dependency----> CustomerName depends on OrderID
-
--- To achieve 2NF we separate into different Tables
-
--- Customers Table
-CREATE TABLE Customers(
-customer_id INT PRIMARY KEY AUTO_INCREMENT,
-CustomerName VARCHAR(100)
+-- Question 2
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100)
 );
 
--- Orders Table
-CREATE TABLE Orders(
-order_id INT PRIMARY KEY AUTO_INCREMENT,
-customer_id INT,
-FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
+INSERT INTO Orders (OrderID, CustomerName)
+VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
+
+
+CREATE TABLE Product (
+    OrderID INT,
+    Product VARCHAR(100),
+    Quantity INT,
+    PRIMARY KEY (OrderID, Product),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
 );
-
--- OrderItems Table
-CREATE TABLE OrderItems(
-orderItem_id INT PRIMARY KEY AUTO_INCREMENT,
-product VARCHAR(100),
-quantity INT,
-order_id INT,
-FOREIGN KEY (order_id) REFERENCES Orders(order_id)
-);
-
--- conclusion No partial dependencies- all non key attributes depends on the primary key of their respective tables.
--- Inserting data into tables
-INSERT INTO Customers (CustomerName) VALUES
-('John Doe'),
-('Jane Smith'),
-('Emily Clark');
-
-INSERT INTO Orders (customer_id) VALUES
-(1), -- OrderID 101
-(2), -- OrderID 102
-(3); -- OrderID 103
-
-INSERT INTO OrderItems (product, quantity, order_id) VALUES
-('Laptop', 2, 1),
-('Mouse', 1, 1),
-('Tablet', 3, 2),
-('Keyboard', 1, 2),
-('Mouse', 2, 2),
-('Phone', 1, 3);
+INSERT INTO Product (OrderID, Product, Quantity)
+VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
 
